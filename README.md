c4d8f5718ced04b2aec3a9b6a52416b2ada1fafb
Evgeny Lukashevich <evgeny@positivedata.se>
2024-05-21 17:28:50 +0200

    initial commit bootstrap

--- README.md
+++
@@ -1,2 +0,0 @@
-# bootstrap-profile
-Initialize minimal applications to setup powershell profile on a clean computer
---
+++ bootstrap.ps1
@@ -0,0 +1,183 @@
+param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [String]$Repo,
+
+    [ValidateNotNullOrEmpty()]
+    [String]$RepoLocation = "./scripts",
+
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet('scoop', 'winget')]
+    [String]$Installer = "scoop",
+
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet('full', 'minimal')]
+    [String]$Preset = "minimal"
+)
+
+function Bootstrap-Profile {
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Repo,
+
+        [ValidateNotNullOrEmpty()]
+        [String]$RepoLocation = "./scripts",
+
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('scoop', 'winget')]
+        [String]$Installer = "scoop",
+
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('full', 'minimal')]
+        [String]$Preset = "minimal"
+    )
+
+    $ErrorActionPreference = "Stop"
+
+    if (Test-Path $RepoLocation) {
+        throw "Repo location already exists, please provide a new location. Path: $RepoLocation"
+    }
+
+    $repoPath = GetAbsolutePath $RepoLocation
+
+    BootstrapProfile -Installer $Installer
+    CloneProfileRepo -RepoName $Repo -RepoLocation $repoPath
+    AddRepoToProfile -RepoName $Repo -RepoLocation $repoPath
+
+    # start a new shell and install apps
+    if ($Installer -eq "scoop") {
+        Start-Process powershell -ArgumentList "-NoExit", "-Command & { Install-ScoopApps -Preset $Preset }"
+    }
+}
+
+function BootstrapProfile ([String]$Installer) {
+    if ($Installer -eq "scoop") {
+        BootstrapUsingScoop
+    }
+    elseif ($Installer -eq "winget") {
+        BootstrapUsingWinGet
+    }
+    else {
+        throw "Invalid installer: $Installer"
+    }
+}
+
+function BootstrapUsingScoop {
+    if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+        Write-Host "Installing Scoop..."
+        Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+        Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+        Write-Host "Scoop installed."
+    }
+
+    Write-Host "Installing git and gh..."
+    scoop install git
+    scoop install gh
+    Write-Host "Installed."
+}
+
+function BootstrapUsingWinGet {
+    throw "Not implemented."
+}
+
+function CloneProfileRepo([String]$RepoName, [String]$RepoLocation) {
+    Write-Host "Cloning $RepoName to $RepoLocation..."
+
+    # check if already logged in into github (supress output and error messages)
+    try {
+        gh auth status *>&1 | Out-Null
+    }
+    catch {
+        if ($LASTEXITCODE -ne 0) {
+            Write-Host "Authenticating with GitHub..."
+            gh auth login
+            if ($LASTEXITCODE -ne 0) {
+                throw "Failed to authenticate with GitHub. Error code: $LASTEXITCODE"
+            }
+        }
+    }
+
+    gh repo clone $RepoName $RepoLocation
+    if ($LASTEXITCODE -ne 0) {
+        throw "Failed to clone $RepoName. Error code: $LASTEXITCODE"
+    }
+
+    Write-Host "Cloned to $RepoLocation."
+
+    if (-not (Test-Path $RepoLocation)) {
+        throw "Something went wrong, repo directory not found on disk. Path: $RepoLocation"
+    }
+
+    # set repo path in env variable
+    SetRepoPath -RepoPath $RepoLocation
+}
+
+function AddRepoToProfile([String]$RepoName, [String]$RepoLocation) {
+    Write-Host "Adding $RepoName to powershell profile..."
+    Write-Host "Repo path: $RepoLocation"
+
+    $profilePath = $PROFILE.CurrentUserAllHosts
+    Write-Host "Profile path: $profilePath"
+
+    CreatePsProfile -ProfilePath $profilePath
+
+    $importStr = ". $RepoLocation\profile.ps1"
+    # add import statement if not exists
+    $escapedImportStr = [Regex]::Escape($importStr)
+    if (-not (Select-String -Path $profilePath -Pattern $escapedImportStr)) {
+        Add-Content $profilePath -Value "`n$importStr"
+    }
+
+    Write-Host "Added."
+}
+
+function SetRepoPath([String]$RepoPath) {
+    if (-not (Test-Path $RepoPath)) {
+        throw "Invalid repo path: $RepoPath"
+    }
+
+    if (-not (Test-Path "Env:\SCRIPT_REPO_ROOT")) {
+        Write-Host "Setting SCRIPT_REPO_ROOT env. variable to '$RepoPath'..."
+        $location = Resolve-Path $RepoPath
+        [System.Environment]::SetEnvironmentVariable('SCRIPT_REPO_ROOT', "$location", 'User')
+        Write-Host "Done."
+    }
+}
+
+function CreatePsProfile {
+    param(
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [String]$ProfilePath
+    )
+
+    # create profile folder if not exists
+    if (-not (Test-Path $profilePath)) {
+        $dir = [System.IO.Path]::GetDirectoryName($profilePath)
+
+        if (-not (Test-Path $dir)) {
+            New-Item -ItemType Directory -Path $dir | Out-Null
+        }
+    }
+
+    # create profile file if not exists
+    if (-not (Test-Path $profilePath)) {
+        New-Item -ItemType File -Path $profilePath | Out-Null
+    }
+}
+
+function GetAbsolutePath([String]$Path) {
+    $isRelative = [System.IO.Path]::IsPathRooted($Path)
+
+    if (-not $isRelative) {
+        $currentDir = Get-Location
+        $joinedPath = Join-Path -Path $currentDir -ChildPath $Path
+
+        return [System.IO.Path]::GetFullPath($joinedPath)
+    }
+
+    return [System.IO.Path]::GetFullPath($Path)
+}
+
+Bootstrap-Profile -Repo $Repo -RepoLocation $RepoLocation -Installer $Installer -Preset $Preset
